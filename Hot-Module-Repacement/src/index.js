f7570120916fb886e445e904b9d7daf321b98aca
李凯泉 <likaiquan@supos.com>
2022-10-20 15:15:09 +0800

    feat: add html-webpack-plugin

--- Hot-Module-Repacement/src/index.js
+++ Hot-Module-Repacement/src/index.js
@@ -1,14 +1,18 @@
 import _ from 'lodash';
+import printMe from './print.js';
 import './style.css';
 import Icon from './icon.png';
 
 function component() {
     const element = document.createElement('div');
-  
+    const btn = document.createElement('button');
      // lodash 在当前 script 中使用 import 引入
     element.innerHTML = _.join(['shell', 'coochi'], ' ');
     element.classList.add('hello');
-
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
+  
+    element.appendChild(btn);
      // 将图像添加到我们已经存在的 div 中。
     const myIcon = new Image();
     myIcon.src = Icon;
