227a9510f60ad7068abb1b019543f6c89dfc6bbe
shellcoochi <18706577758@163.com>
2022-11-01 22:09:46 +0800

    feat: 开发环境

--- Hot-Module-Repacement/webpack.config.js
+++ Hot-Module-Repacement/webpack.config.js
@@ -2,34 +2,43 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: {
-    index: './src/index.js',
-    print: './src/print.js',
-  },
-  plugins: [
-    new HtmlWebpackPlugin({//生成index.html 文件
-      title: '管理输出',
-    }),
-  ],
-  output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true,//清理 /dist 文件夹
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: 'asset/resource',
-      },
+    mode: 'development',//配置开发环境
+    entry: {
+        index: './src/index.js',
+        print: './src/print.js',
+    },
+    devtool: 'inline-source-map',//配置source maps
+    devServer: {
+        static: './dist',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({//生成index.html 文件
+            title: '开发环境',
+        }),
     ],
-  },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,//清理 /dist 文件夹
+        publicPath: '/',
+    },
+    optimization: {
+        runtimeChunk: 'single',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
 };
